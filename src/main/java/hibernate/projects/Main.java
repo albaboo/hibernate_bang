3685d20f633f2e0263750c68c08ae4e94c1a4e9f
albaboo <smx.bazan.alba@gmail.com>
2025-10-23 17:13:10 +0200

    #

--- src/main/java/hibernate/projects/Main.java
+++ src/main/java/hibernate/projects/Main.java
@@ -1,10 +1,5 @@
 package hibernate.projects;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -12,19 +7,19 @@ import jakarta.persistence.PersistenceException;
 
 public class Main {
 
-	private static EntityManagerFactory entityManagerFactory;
+    private static EntityManagerFactory entityManagerFactory;
 
-	public static EntityManagerFactory getEntityManagerFactory() {
-		try {
-			if (entityManagerFactory == null) {
-				entityManagerFactory = Persistence.createEntityManagerFactory("cineversePU"); //El nom s'ha de correspondre amb el de l'arxiu persistence.xml.
-			}
-		} catch (PersistenceException e) {
-			System.err.println("Error al construir l'EntityManagerFactory: " + e.getMessage());
-			throw new ExceptionInInitializerError(e);
-		}
-		return entityManagerFactory;
-	}
+    public static EntityManagerFactory getEntityManagerFactory() {
+        try {
+            if (entityManagerFactory == null)
+                entityManagerFactory = Persistence.createEntityManagerFactory("cineversePU");
+
+        } catch (PersistenceException e) {
+            System.err.println("Error al construir l'EntityManagerFactory: " + e.getMessage());
+            throw new ExceptionInInitializerError(e);
+        }
+        return entityManagerFactory;
+    }
 
     public static void main(String[] args) {
         EntityManager entityManager = null;
@@ -39,16 +34,6 @@ public class Main {
 
             if (entityManagerFactory != null && entityManagerFactory.isOpen())
                 entityManagerFactory.close();
-        }
-
-        try (Session session = getSessionFactory().openSession()) {
-
-        } catch (Exception e) {
-            System.out.println("Error durant l'execució del programa " + e.getMessage());
-        } finally {
-            if (sessionFactory != null && sessionFactory.isOpen()) {
-                sessionFactory.close();
-            }
         }
 
     }
