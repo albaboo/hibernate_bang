acce8f710bca713218bcaff4ca2c1151f0f40091
albaboo <smx.bazan.alba@gmail.com>
2025-10-26 15:16:19 +0100

    added descriptions to type cards

--- src/main/java/hibernate/projects/Enum/TypeUse.java
+++ src/main/java/hibernate/projects/Enum/TypeUse.java
@@ -1,5 +1,13 @@
 package hibernate.projects.Enum;
 
 public enum TypeUse {
-    BANG, FAILED, BEER
+    BANG("Carta de ataque; se utiliza para hacer daño a un jugador a distancia válida."),
+    FAILED("Carta de defensa; puede anular un BANG si se juega a tiempo."),
+    BEER("Carta de curación; recupera 1 punto de vida en el jugador que la juega.");
+
+    public final String description;
+
+    TypeUse(String description) {
+        this.description = description;
+    }
 }
