9c2379877b5bb1e3ae8c44bcc5c7228eec20bbf8
albaboo <smx.bazan.alba@gmail.com>
2025-10-26 13:02:19 +0100

    #

--- src/main/java/hibernate/projects/Enum/TypeRole.java
+++ src/main/java/hibernate/projects/Enum/TypeRole.java
@@ -2,7 +2,7 @@ package hibernate.projects.Enum;
 
 public enum TypeRole {
     SHERIFF("Los malhechores y el renegado deben quedar eliminados. Identidad publica desde el principio."),
-    RENEGADE("Ser el último jugador vivo junto al sheriff."), 
+    RENEGADE("Ser el último jugador vivo junto al sheriff."),
     MALFACTOR("El sheriff debe quedar eliminado."),
     ASSISTANT("Los malhechores y el renegado deben quedar eliminados. Ayuda al sheriff.");
 
